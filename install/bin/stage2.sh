332e4d014d097530057ea9d0438c109e1fa46c69
Steve Norledge <steve@honeydew.local>
2016-07-10 21:58:27 +0100

    - add ipa packages install

--- install/bin/stage2.sh
+++ install/bin/stage2.sh
@@ -3,6 +3,7 @@
 . /etc/symphony.cfg
 
 SOURCE=1
+IPA=1
 
 YUMBASE=/opt/symphony/generic/etc/yum/centos7-base.conf
 
@@ -11,9 +12,32 @@ if [ $SOURCE -gt 0 ]; then
   mkdir /opt/symphony
   cd /opt/symphony
   git clone https://github.com/alces-software/symphony4.git generic
-  git clone https://github.com/alces-software/symphony-director.git director
+  git clone https://github.com/alces-software/symphony-directory.git directory
 fi
 ############# END SOURCE ###################
+
+############# BEGIN IPA ###################
+if [ $IPA -gt 0 ]; then
+  yum --config=$YUMBASE -y install ipa-server bind bind-dyndb-ldap ipa-server-dns
+  firewall-cmd --add-service ssh --zone prv --permanent
+  firewall-cmd --add-service ssh --zone bld --permanent
+  firewall-cmd --add-service http --zone prv --permanent
+  firewall-cmd --add-service https --zone prv --permanent
+  firewall-cmd --add-service ldap --zone prv --permanent
+  firewall-cmd --add-service ldaps --zone prv --permanent
+  firewall-cmd --add-service kerberos --zone prv --permanent
+  firewall-cmd --add-service kpasswd --zone prv --permanent
+  firewall-cmd --add-service dns --zone prv --permanent
+  firewall-cmd --add-service http --zone bld --permanent
+  firewall-cmd --add-service https --zone bld --permanent
+  firewall-cmd --add-service ldap --zone bld --permanent
+  firewall-cmd --add-service ldaps --zone bld --permanent
+  firewall-cmd --add-service kerberos --zone bld --permanent
+  firewall-cmd --add-service kpasswd --zone bld --permanent
+  firewall-cmd --add-service dns --zone bld --permanent
+  firewall-cmd --reload
+fi
+############# END IPA ###################
 
 #fix resolv.conf
   cat << EOF > /etc/resolv.conf
