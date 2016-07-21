1f1e46ee2b471957b115594a4a43ec5f7c2af305
Vaughan Jones <vaughan.jones@alces-software.com>
2016-07-21 23:02:45 +0100

    Update IPA join script after testing

--- bin/cloud/enrol-ipa.sh
+++ bin/cloud/enrol-ipa.sh
@@ -7,12 +7,13 @@ ONETIMEPASS=moose
 
 DOMAIN=`hostname -d | cut -d . -f 2-3`
 REALM=$(echo `hostname -d` | sed -e 's/\(.*\)/\U\1/')
+DIRECTORYIP="10.75.10.3"
 DIRECTORY="directory.$CLUSTER.$DOMAIN"
 
 #STOP DHCP CHANGING RESOLV.CONF ON REBOOTS
 sed -i -e "s/^PEERDNS.*$/PEERDNS=\"no\"/g" /etc/sysconfig/network-scripts/ifcfg-eth0
 #PREP RESOLVE.CONF
-echo -e "search $CLUSTER.$DOMAIN $DOMAIN\nnameserver $DIRECTORY_IP" > /etc/resolv.conf
+echo -e "search $CLUSTER.$DOMAIN $DOMAIN\nnameserver " > /etc/resolv.conf
 
 yum -y install ipa-client ipa-admintools
-ipa-client-install --no-ntp --mkhomedir --force-join --realm="$REALM" --server="${DIRECTORY}" -w "$ONETIMEPASS" --domain="${CLUSTER}.${DOMAIN}" --unattended
+ipa-client-install --no-ntp --mkhomedir --force-join --realm="$REALM" --server="directory.${CLUSTER}.${DOMAIN}" -w "$ONETIMEPASS" --domain="${CLUSTER}.${DOMAIN}" --unattended
