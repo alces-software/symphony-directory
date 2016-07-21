15fed634ea0256c7a4284626f17d87a43d21e9ed
Vaughan Jones <vaughan.jones@alces-software.com>
2016-07-21 22:33:34 +0100

    Updates to add cluster script

--- bin/cloud/add-cluster.sh
+++ bin/cloud/add-cluster.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-CLUSTER=cluster1
 KEYTAB=/root/admin.keytab
 KEYUSER=admin
 
 DIRECTORYAPPLIANCE=`hostname -f`
 
-DOMAIN=`hostname -d`
+DOMAIN=`hostname -d | cut -d . -f 2-3`
 REALM=`echo $DOMAIN | sed -e 's/\(.*\)/\U\1/'`
+
+if [ ! -z $CLUSTER ];
+then
+    CLUSTER=$1
+fi
 
 kinit -kt $KEYTAB $KEYUSER@$REALM
 
-ipa dnszone-add $CLUSTER.$DOMAIN --name-server $DIRECTORYAPPLIANCE.
+ipa dnszone-add $DOMAIN --name-server $DIRECTORYAPPLIANCE.
 ipa dnsrecord-add $DOMAIN $CLUSTER --ns-rec=$DIRECTORYAPPLIANCE.
 
 kdestroy
