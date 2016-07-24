f0c09c1f4d96541bac2db914955b0d19e41bbce7
Vaughan Jones <vaughan.jones@alces-software.com>
2016-07-24 11:55:42 +0100

    Added initial remove cluster script

---
+++ bin/cloud/remove-cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+KEYTAB=/root/admin.keytab
+KEYUSER=admin
+
+CLUSTER=$1
+
+if [ -z $CLUSTER ];
+then
+  echo "No cluster specified"
+  echo "Usage: $0 <cluster name>"
+  exit 1
+fi
+
+DIRECTORYAPPLIANCE=`hostname -f`
+DOMAIN=`hostname -d`
+REALM=$(echo $DOMAIN | sed -e 's/\(.*\)/\U\1/')
+
+kinit -kt $KEYTAB $KEYUSER@$REALM
+
+ipa dnsrecord-del $REALM $CLUSTER --del-all
+
+kdestroy
