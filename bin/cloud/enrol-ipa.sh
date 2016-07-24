330a79774e7075532181efab4a95a413a08801c9
Vaughan Jones <vaughan.jones@alces-software.com>
2016-07-24 11:32:55 +0100

    Update IPA enrol script

--- bin/cloud/enrol-ipa.sh
+++ bin/cloud/enrol-ipa.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+set -ex
 CLIENTNAME=`hostname -s`
 CLUSTER=`hostname -d | cut -d . -f 1`
 CLIENTIP=`hostname -i`
 ONETIMEPASS=moose
 
-DOMAIN=`hostname -d | cut -d . -f 2-3`
-REALM=$(echo `hostname -d` | sed -e 's/\(.*\)/\U\1/')
-DIRECTORYIP="10.75.10.3"
-DIRECTORY="directory.$CLUSTER.$DOMAIN"
+DOMAIN=`hostname -d | cut -d . -f 2-4`
+REALM=$(echo $DOMAIN | sed -e 's/\(.*\)/\U\1/')
+DIRECTORYIP="10.75.0.7"
+DIRECTORY="directory.$DOMAIN"
 
 #STOP DHCP CHANGING RESOLV.CONF ON REBOOTS
 sed -i -e "s/^PEERDNS.*$/PEERDNS=\"no\"/g" /etc/sysconfig/network-scripts/ifcfg-eth0
@@ -16,4 +16,4 @@ sed -i -e "s/^PEERDNS.*$/PEERDNS=\"no\"/g" /etc/sysconfig/network-scripts/ifcfg-
 echo -e "search $CLUSTER.$DOMAIN $DOMAIN\nnameserver $DIRECTORYIP" > /etc/resolv.conf
 
 yum -y install ipa-client ipa-admintools
-ipa-client-install --no-ntp --mkhomedir --force-join --realm="$REALM" --server="directory.${CLUSTER}.${DOMAIN}" -w "$ONETIMEPASS" --domain="${CLUSTER}.${DOMAIN}" --unattended
+ipa-client-install --no-ntp --mkhomedir --force-join --realm="$REALM" --server="${DIRECTORY}" -w "$ONETIMEPASS" --domain="${CLUSTER}.${DOMAIN}" --unattended
