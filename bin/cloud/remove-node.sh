8238c58299f265b32a388ab243a11d0a17c85c61
Vaughan Jones <vaughan.jones@alces-software.com>
2016-07-24 12:15:58 +0100

    Remove a nodes entry from the reverse zone when deleting

--- bin/cloud/remove-node.sh
+++ bin/cloud/remove-node.sh
@@ -14,10 +14,15 @@ KEYUSER=hadder
 
 DOMAIN=$(hostname -d | cut -d '.' -f 2-3)
 REALM=$(echo `hostname -d` | sed -e 's/\(.*\)/\U\1/')
+REVERSEZONE="`hostname -i | cut -d . -f 3`.`hostname -i | cut -d . -f 2`.`hostname -i | cut -d . -f 1`.in-addr.arpa."
 
 kinit -kt $KEYTAB $KEYUSER@$REALM
 
 ipa host-del $CLIENTNAME.$CLUSTER.$REALM --updatedns --continue
 ipa dnsrecord-del $CLUSTER.$REALM $CLIENTNAME --del-all
+
+# Work out the record number for client 
+RECORDNUMBER=$(ipa dnsrecord-find 0.75.10.in-addr.arpa. | grep -B 1 "$CLIENTNAME.$CLUSTER.$REALM" | grep "Record name:" | awk '{print $3}')
+ipa dnsrecord-del $REVERSEZONE $RECORDNUMBER --del-all
 
 kdestroy
