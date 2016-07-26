0b7dd242d7ed602015dae8d9480f5bc44bf3d610
Vaughan Jones <vaughan.jones@alces-software.com>
2016-07-26 17:05:41 +0100

    Update `remove-cluster` for use with Trigger service

--- bin/cloud/remove-cluster.sh
+++ bin/cloud/remove-cluster.sh
@@ -4,21 +4,33 @@ KEYTAB=/root/admin.keytab
 KEYUSER=admin
 
 CLUSTER=$1
-
-if [ -z $CLUSTER ];
-then
-  echo "No cluster specified"
-  echo "Usage: $0 <cluster name>"
-  exit 1
-fi
-
 DIRECTORYAPPLIANCE=`hostname -f`
 DOMAIN=`hostname -d`
 REALM=$(echo $DOMAIN | sed -e 's/\(.*\)/\U\1/')
 
-kinit -kt $KEYTAB $KEYUSER@$REALM
+function check_cluster {
+    if [ "$(ipa dnszone-find | grep "${CLUSTER}.${DOMAIN}." | awk '{print $3}')" == "${CLUSTER}.${DOMAIN}." ];
+    then
+        remove_cluster >> /dev/null
+        echo "OK"
+    else
+        echo "CLUSTER ${CLUSTER} DOES NOT EXIST"
+        exit 0
+    fi
+}
 
-ipa dnsrecord-del $REALM $CLUSTER --del-all
-ipa dnszone-del $CLUSTER.$REALM
+function remove_cluster {
+    ipa dnsrecord-del $REALM $CLUSTER --del-all
+    ipa dnszone-del $CLUSTER.$REALM
+}
 
-kdestroy
+if [ -z $CLUSTER ];
+then
+    echo "No cluster specified"
+    echo "Usage: $0 <cluster name>"
+    exit 0
+else
+    kinit -kt $KEYTAB $KEYUSER@$REALM
+    check_cluster
+    kdestroy
+fi
