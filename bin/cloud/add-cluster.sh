bc4626c63e2731600bef6269c7bc790b17f9a857
Vaughan Jones <vaughan.jones@alces-software.com>
2016-07-26 16:49:44 +0100

    Update `add-cluster` for use with Trigger service

--- bin/cloud/add-cluster.sh
+++ bin/cloud/add-cluster.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
-
+set -ex
 KEYTAB=/root/admin.keytab
 KEYUSER=admin
 
 CLUSTER=$1
-
-if [ -z $CLUSTER ];
-then
-  echo "No cluster specified"
-  echo "Usage: $0 <cluster name>"
-  exit 1
-fi
-
 DIRECTORYAPPLIANCE=`hostname -f`
 DOMAIN=`hostname -d`
 REALM=$(echo $DOMAIN | sed -e 's/\(.*\)/\U\1/')
 
-kinit -kt $KEYTAB $KEYUSER@$REALM
+function check_cluster {
+    if [ "$(ipa dnszone-find | grep "${CLUSTER}.${DOMAIN}." | awk '{print $3}')" == "${CLUSTER}.${DOMAIN}." ];
+    then
+        echo "FAILED"
+        exit 1
+    else
+        add_cluster >> /dev/null
+        echo "OK"
+    fi
+}
 
-ipa dnszone-add $CLUSTER.$DOMAIN --name-server $DIRECTORYAPPLIANCE.
-ipa dnsrecord-add $DOMAIN $CLUSTER --ns-rec=$DIRECTORYAPPLIANCE.
+function add_cluster {
+    ipa dnszone-add $CLUSTER.$DOMAIN --name-server $DIRECTORYAPPLIANCE.
+    ipa dnsrecord-add $DOMAIN $CLUSTER --ns-rec=$DIRECTORYAPPLIANCE.
+}
 
-kdestroy
+if [ -z $CLUSTER ];
+then
+    echo "No cluster specified"
+    echo "Usage: $0 <cluster name>"
+    exit 0
+else
+    kinit -kt $KEYTAB $KEYUSER@$REALM
+    check_cluster
+    kdestroy
+fi
