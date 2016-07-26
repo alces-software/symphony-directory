0636899413e804e06dc7b6918ae11526b8668358
Vaughan Jones <vaughan.jones@alces-software.com>
2016-07-26 18:07:05 +0100

    Updated `remove-node` for use with Trigger service

--- bin/cloud/remove-node.sh
+++ bin/cloud/remove-node.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
-set -ex
+
 CLIENTNAME=$1
 CLUSTER=$2
 
-if [[ -z $CLIENTNAME || -z $CLUSTER ]];
-then
-    echo "Usage: $0 <client name> <cluster>"
-    exit 1
-fi
-
 KEYTAB=/root/hadder.keytab
 KEYUSER=hadder
-
 DOMAIN=$(hostname -d | cut -d '.' -f 2-3)
 REALM=$(echo `hostname -d` | sed -e 's/\(.*\)/\U\1/')
 REVERSEZONE="`hostname -i | cut -d . -f 3`.`hostname -i | cut -d . -f 2`.`hostname -i | cut -d . -f 1`.in-addr.arpa."
 
-kinit -kt $KEYTAB $KEYUSER@$REALM
+function check_node {
 
-ipa host-del $CLIENTNAME.$CLUSTER.$REALM --updatedns --continue
-ipa dnsrecord-del $CLUSTER.$REALM $CLIENTNAME --del-all
+    $DOMAINRECORDS=$(ipa dnsrecord-find $CLUSTER.$DOMAIN $CLIENTNAME |
+                     grep "${CLIENTNAME}" |
+                     awk '{print $3}')
+    if [[ ! $DOMAINRECORDS == $CLIENTNAME || ! $REALMRECORDS == $CLIENTNAME ]];
+    then
+        echo "FAIL - NO ENTRY EXISTS"
+        exit 0
+    else
+        remove_node >> /dev/null
+        echo "OK"
+    fi
 
-# Work out the record number for client 
-RECORDNUMBER=$(ipa dnsrecord-find 0.75.10.in-addr.arpa. | grep -B 1 "$CLIENTNAME.$CLUSTER.$REALM" | grep "Record name:" | awk '{print $3}')
-ipa dnsrecord-del $REVERSEZONE $RECORDNUMBER --del-all
+}
 
-kdestroy
+function remove_node {
+
+    ipa host-del $CLIENTNAME.$CLUSTER.$REALM --updatedns --continue
+    ipa dnsrecord-del $CLUSTER.$REALM $CLIENTNAME --del-all
+
+    # Work out the record number for client
+    RECORDNUMBER=$(ipa dnsrecord-find $REVERSEZONE |
+                   grep -B 1 "$CLIENTNAME.$CLUSTER.$REALM" |
+                   grep "Record name:" |
+                   awk '{print $3}')
+    ipa dnsrecord-del $REVERSEZONE $RECORDNUMBER --del-all
+}
+
+if [[ -z $CLIENTNAME || -z $CLUSTER ]];
+then
+    echo "One or more settings not provided"
+    echo "Usage: $0 <client name> <cluster>"
+    exit 0
+else
+    kinit -kt $KEYTAB $KEYUSER@$REALM
+    check_node
+    kdestroy
+fi
